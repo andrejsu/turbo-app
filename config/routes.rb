3891abb06187fb39ac10521f565f891d2645f30f
Andrew Sukhikh <sukhikh.andrew@gmail.com>
2022-07-14 16:54:28 +0300

    Added header and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   devise_scope :user do
     # Redirests signing out users back to sign-in
     get "users", to: "devise/sessions#new"
   end
 
   devise_for :users
+
+  root 'pages#home'
 end
